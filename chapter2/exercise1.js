488d785160a083d22eda05c9a531001c01b3ba61
shakirasultana200-rgb <shakirasultana200gmail.com>
2026-07-27 22:41:28 +0600

    message

---
+++ chapter2/exercise1.js
@@ -0,0 +1,5 @@
+const name = "Shakira Sultana";
+const department = "Computer Science & Engineering (CSE)";
+let semester = "3rd Year, 1st Semester";
+
+console.log(`My name is ${name}. I study in the ${department} department and I am currently in ${semester}.`);
