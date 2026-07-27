b04cf77d69fcc8b0febbbb465be5c17d9cb13215
shakirasultana200-rgb <shakirasultana200gmail.com>
2026-07-27 22:59:19 +0600

    message

---
+++ chapter3/exercise4.js
@@ -0,0 +1,30 @@
+const month = 7;
+
+switch (month) {
+    case 3:
+    case 4:
+    case 5:
+        console.log("Summer");
+        break;
+
+    case 6:
+    case 7:
+    case 8:
+        console.log("Monsoon");
+        break;
+
+    case 9:
+    case 10:
+    case 11:
+        console.log("Autumn");
+        break;
+
+    case 12:
+    case 1:
+    case 2:
+        console.log("Winter");
+        break;
+
+    default:
+        console.log("Invalid Month");
+}
