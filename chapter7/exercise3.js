f1ffda6ee82edba29941bb52af16cad42e5a9e12
shakirasultana200-rgb <shakirasultana200gmail.com>
2026-07-29 21:30:58 +0600

    message

---
+++ chapter7/exercise3.js
@@ -0,0 +1,10 @@
+const student = {
+    name: "Shakira Sultana",
+    dept: "CSE",
+    semester: "3rd Year, 1st Semester"
+};
+
+const { name, semester } = student;
+
+console.log(name);
+console.log(semester);
