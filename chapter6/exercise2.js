7b07039efb68ac695a9761f1a4a86b173a468953
shakirasultana200-rgb <shakirasultana200gmail.com>
2026-07-29 21:19:57 +0600

    message

--- chapter6/exercise2.js
+++ chapter6/exercise2.js
@@ -1,5 +1,5 @@
 const amounts = [450, 200, 1000, 750];
 
-const taka = amounts.map(amount => `${amount} BDT`);
+const taka = amounts.map(amount => '${amount} BDT');
 
 console.log(taka);
---
+++ chapter6/exercise3.js
@@ -0,0 +1,5 @@
+const names = ["Ali", "Rahim", "Karim", "Mim", "Shakira", "Nusrat"];
+
+const longNames = names.filter(name => name.length > 4);
+
+console.log(longNames);
