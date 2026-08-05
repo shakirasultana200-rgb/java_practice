936e3271a81ce30634b44f68d9dad5ae554214b0
shakirasultana200-rgb <shakirasultana200gmail.com>
2026-08-05 19:10:40 +0600

    message

--- chapter15/exercise3.js
+++ chapter15/exercise3.js
@@ -1,34 +1,11 @@
-function selectionSort(arr) {
+const students = [
+    { name: "Rahim", cgpa: 3.50 },
+    { name: "Karim", cgpa: 3.90 },
+    { name: "Nusrat", cgpa: 3.70 }
+];
 
-    const a = [...arr];
+students.sort((a, b) => b.cgpa - a.cgpa);
 
-    let comparisons = 0;
-
-    for (let i = 0; i < a.length - 1; i++) {
-
-        let minIndex = i;
-
-        for (let j = i + 1; j < a.length; j++) {
-
-            comparisons++;
-
-            if (a[j] < a[minIndex]) {
-                minIndex = j;
-            }
-
-        }
-
-        if (minIndex !== i) {
-            [a[i], a[minIndex]] = [a[minIndex], a[i]];
-        }
-
-    }
-
-    console.log("Comparisons:", comparisons);
-
-    return a;
-}
-
-selectionSort([5, 4, 3, 2, 1]);
-selectionSort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
-selectionSort([20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
+students.forEach(student => {
+    console.log(student.name, student.cgpa);
+});
