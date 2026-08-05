199a8d489659e52be4098186f2fd465dbcfd9c53
shakirasultana200-rgb <shakirasultana200gmail.com>
2026-08-05 19:34:32 +0600

    message

---
+++ chapter17/exercise3.js
@@ -0,0 +1,11 @@
+const student = {
+    name: "Shakira",
+    dept: "CSE",
+    semester: "8th",
+    skills: ["HTML", "CSS", "JavaScript"]
+};
+
+const { name, semester } = student;
+
+console.log(name);
+console.log(semester);
